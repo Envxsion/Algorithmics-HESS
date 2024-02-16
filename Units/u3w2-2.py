75a28b251bd5ebf2d14951becd18a196e9ff5cb6
Envxsion <74938513+Envxsion@users.noreply.github.com>
2024-02-16 20:47:59 +1100

    wk3

--- Units/u3w2-2.py
+++ Units/u3w2-2.py
@@ -1,7 +1,6 @@
 '''You will write functions to convert between decimal and binary representation in Python.'''
 def dec2bin(n):
     #return bin(n).replace("0b", "") is a oneliner that could be used instead
-    print(bin(n).replace("0b", ""))
     k=[]
     while (n>0):
         k.append(int(float(n%2)))
@@ -10,7 +9,13 @@ def dec2bin(n):
     string = ''.join(str(k) for k in k)
     return string
 
-def bin2dec(n):
-    return int(n, 2)
+def bin2dec(m):
+    #return the int(m, 2) oneliner works too
+    dec= 0
+    for digit in m:
+        dec= dec* 2 + int(digit)
+    return dec
+
+
 print(dec2bin(int(input("Enter a number to conv to binary: "))))
 print(bin2dec(input("Enter a number to conv to decimal: ")))
---
+++ Units/u3w3-1.py
@@ -0,0 +1,15 @@
+'''A prison contains a large number of cells, numbered sequentially from 1 to 500. One night, when the prisoners are asleep, a bored guard unlocks every cell. Then, he returns to the start. He stops at every cell that is a multiple of two. If the cell is unlocked, he locks it. If it is locked, he unlocks it. He repeats this process for multiples of three, then four, and so on.
+'''
+cells = [False] * 501
+
+for i in range(2, 501):
+    for j in range(i, 501, i):
+        cells[j] = not cells[j]
+print(cells[225])
+print(cells[484])
+print(cells[170])
+print(cells[499])
+print("Locked Cells: ", sum(1 for cell in cells if cell))
+
+
+
