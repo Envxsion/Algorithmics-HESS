fc34540d2ec444c558b86186548683326c66917b
Envxsion <74938513+Envxsion@users.noreply.github.com>
2024-03-19 14:23:12 +1100

    kinda works

--- Units/u3w6-1.py
+++ Units/u3w6-1.py
@@ -40,22 +40,21 @@ class Edge:
 class RandomGraph:
 
   def __init__(self,V_N,E_N):
-    # Creates a random graph objects with V_N vertices each with E_N edges
     self.edges = []
     self.nodes = [Node(i) for i in range(1,V_N+1)]
     
     for node in self.nodes:
-      neighbours = random.sample(self.nodes,3)
+      neighbours = random.choices(self.nodes, k=3)
       for neighbour in neighbours:
-        if node != neighbour and self.get_weight(node.id, neighbour.id) == -1:
-          rw = random.randint(1,7)
-          edge = Edge(node, neighbour, rw)
-          self.edges.append(edge)
-          node.add_neighbour(neighbour)
-          neighbour.add_neighbour(node)
+          if node != neighbour and self.get_weight(node.id, neighbour.id) == -1:
+              rw = random.randint(1, 7)
+              edge = Edge(node, neighbour, rw)
+              self.edges.append(edge)
+              node.add_neighbour(neighbour)
+              neighbour.add_neighbour(node)
+
 
   def get_weight(self, node1, node2):
-    # Returns the edge weight between two nodes
     for edge in self.edges:
       if edge.id1 == node1 and edge.id2 == node2:
         return edge.weight
@@ -64,7 +63,6 @@ class RandomGraph:
     return -1
 
   def set_colour(self, id1, id2, colour):
-    # Sets the edge between id1 and id2 to the stated colour
     for edge in self.edges:
       if edge.id1 == id1 and edge.id2 == id2:
         edge.color = colour
@@ -72,30 +70,27 @@ class RandomGraph:
         edge.color = colour  
 
   def display_graph(self):
-    
     self.G = nx.Graph()
     for node in self.nodes:
-      self.G.add_node(node)
-    for edge in self.edges:
-      self.G.add_edge(edge.source, edge.target, weight = edge.weight)
-    pos = nx.spring_layout(self.G, seed=42)
-    nx.draw(self.G, pos, with_labels=True, node_size=500, node_color='lightblue', font_size=12, font_color='black', font_weight='bold')
-      
-    #edge_labels = nx.get_edge_attributes(self.G, 'weight')
-    edge_labels = {}
+        self.G.add_node(node)
+    
     edge_colors = []
     for edge in self.edges:
-      edge_colors.append(edge.color)  
-      edge_labels[(edge.source, edge.target)]= edge.weight
-    
-    
-    
-    nx.draw(self.G, pos, with_labels=True, node_size=500, node_color='lightblue', font_size=12, font_color='black', font_weight='bold', edge_color=edge_colors, width=2.0)
+        self.G.add_edge(edge.source, edge.target, weight=edge.weight)
+        edge_colors.append(edge.color)
+
+    pos = nx.spring_layout(self.G, seed=42)
+    nx.draw(self.G, pos, with_labels=True, node_size=500, node_color='lightblue',
+            font_size=12, font_color='black', font_weight='bold', edge_color=edge_colors, width=2.0)
+
+    # Draw edge labels
+    edge_labels = {(edge.source, edge.target): edge.weight for edge in self.edges}
     nx.draw_networkx_edge_labels(self.G, pos, edge_labels=edge_labels)
 
     plt.title("Prims")
     plt.axis('off')
     plt.show()
+
 
   def list_nodes_and_edges(self):
     for node in self.nodes:
@@ -109,36 +104,40 @@ class RandomGraph:
       node.visited = False
 
   def Prims(self):
-    self.tree_nodes = [self.nodes[0]]
-    self.tree_edges = []
+    self.reset_visits()  
+    visited_nodes = set()  
+    mst_edges = [] 
+    start_node = random.choice(self.nodes)
+    start_node.visited = True
+    visited_nodes.add(start_node)
     
-    self.reset_visits()
-    self.nodes[0].visited = True
+    while len(visited_nodes) < len(self.nodes):
+        min_edge = None
+        min_weight = float('inf')
+        for node in visited_nodes:
+            for neighbor in node.neighbours:
+                if neighbor not in visited_nodes:
+                    weight = self.get_weight(node.id, neighbor.id)
+                    if weight < min_weight:
+                        min_weight = weight
+                        min_edge = (node, neighbor, weight)
 
-    while len(self.tree_nodes) < len(self.nodes):
-        smallest = float('inf')
-        best_edge = None
-        for node in self.tree_nodes:
-            for neighbour in node.neighbours:
-                if not neighbour.visited:
-                    ew = self.get_weight(node.id, neighbour.id)
-                    if ew < smallest:
-                        next_node = neighbour
-                        id1 = node.id
-                        id2 = neighbour.id
-                        smallest = ew
-                        best_edge = (id1, id2, smallest)
-        
-        self.tree_nodes.append(next_node)
-        next_node.visited = True
-        self.tree_edges.append(best_edge)
-        self.set_colour(best_edge[0], best_edge[1], 'red')
-        print(f"Edge added between {best_edge[0]} and {best_edge[1]} with weight {best_edge[2]}")
+        if min_edge:
+            mst_edges.append(min_edge)
+            neighbor = min_edge[1]
+            neighbor.visited = True
+            visited_nodes.add(neighbor)
+
+    
+    for edge in mst_edges:
+        edge[0].add_neighbour(edge[1])  
+        self.set_colour(edge[0].id, edge[1].id, 'red')
+
+
+
+
       
 RG = RandomGraph(10,3)
-
 RG.Prims()
-
 RG.list_nodes_and_edges()
-
 RG.display_graph()
